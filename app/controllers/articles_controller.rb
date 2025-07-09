e4543979d32b1ec5e471ef9ce87932fd9a600331
MUHAMMAD ARBAZ <MUHAMMADARBAZ3@BAGH.MTBC.COM>
2025-07-09 01:24:08 -0400

    Added Foam Validations and new article creation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,30 +1,32 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
-  end
+ 
 
   def show
     @article = Article.find(params[:id])
   end
 
+   def index
+    @articles = Article.all
+  end
   def new
-    # @article = Article.new
+    @article = Article.new
   end
 
   def create
-    render plain: params[:article]
+      @article = Article.new(article_params)
+     puts "Creating article with params: #{article_params.inspect}"
+puts @article.errors.full_messages.inspect
 
-    # @article = Article.new(article_params)
-    # if @article.save
-    #   redirect_to @article, notice: 'Article was successfully created.'
-    # else
-    #   render :new
-    # end
+    if @article.save
+      redirect_to @article, notice: 'Article was successfully created.'
+    else
+      render :new
+    end
   end
 
-  # private
+  private
 
-  # def article_params
-  #   params.require(:article).permit(:title, :description)
-  # end
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
